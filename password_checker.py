c5ff8c9aff0303856f7320a1831e2b3c7cd9f5f3
jbmasemza <jbmasemza@yahoo.com>
2019-12-12 15:32:36 +0200

    final commit
    final commit

--- password_checker.py
+++ password_checker.py
@@ -2,26 +2,27 @@ import re
 
 # Defining  the function for the password
 password = 'Joyous@1234'
+
 def password_is_valid(password):
     if len(password) == 0:
         raise Exception("password should exist")
     else: 
         if len(password)<=8:
-            
             raise Exception("password should be longer than 8 characters")
         
         elif not re.search('[a-z]', password):
-            raise Exception("password should contain a lowercase character")
+            raise Exception("password should have a lowercase character")
             
         elif not re.search('[A-Z]', password):
-            raise Exception ("password should contain an uppercase character")
+            raise Exception ("password should have an uppercase character")
     
         elif not re.search('[0-9]', password):
-            raise Exception("password should contain a digit") 
+            raise Exception("password should have a digit")
+         
         else:
-            if not re.search("[$&+,:;=?@#|'<>.-^*()%!]", password):
+            if not re.search('[!@#$%^}{::;"\|:"\<,.>/?]', password):
                 raise Exception("password should have at least one special character")
-        
+
 password_is_valid(password)
 
 def password_is_ok(password):
@@ -32,7 +33,7 @@ def password_is_ok(password):
         exceptions = exceptions.split(', ')
         if "password should exist" in exceptions or "password should be longer than 8 characters" in exceptions:
             return False
-        if len(exceptions) <= 2: return True
+        if len(exceptions) <= 2: return False
    else:
         return True   
 password_is_ok(password)
--- test_password_checker.py
+++ test_password_checker.py
@@ -11,16 +11,20 @@ def test_password_length():
 		password_is_valid("Jo13")
 
 def test_password_has_lower():
-	with pytest.raises(Exception, match = "password should contain a lowercase character"):
+	with pytest.raises(Exception, match = "password should have a lowercase character"):
 		password_is_valid("JOY12345678")
 
 def test_password_has_upper():
-	with pytest.raises(Exception, match = "password should contain an uppercase character"):
-		password_is_valid("joy12345678")
+	with pytest.raises(Exception, match = "password should have an uppercase character"):
+		password_is_valid("joy123@45678")
 
 def test_password_has_digit():
-	with pytest.raises(Exception, match = "password should contain a digit"):
+	with pytest.raises(Exception, match = "password should have a digit"):
 		password_is_valid("Joymasemola")
+
+def test_password_has_special_character():
+    with pytest.raises(Exception, match = "password should have at least one special character"):
+        password_is_valid("Joyjk123456")
 
 def test_password_is_ok():
 	assert password_is_ok("Jo13") == False
