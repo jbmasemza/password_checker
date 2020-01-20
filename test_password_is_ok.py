4e8228882d8812ae75df91f4da3a7c447955b84c
jbmasemza <jbmasemza@yahoo.com>
2020-01-20 14:50:03 +0200

    Delete test_password_is_ok.py

--- test_password_is_ok.py
+++
@@ -1,8 +0,0 @@
-from password_checker import password_is_ok
-import pytest
-
-def test_password_is_ok():
-	assert password_is_ok("Jo13") == False
-	assert password_is_ok("J") == False
-	assert password_is_ok("") == False
-	assert password_is_ok("Joyous@123") == True
